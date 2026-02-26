69836d5fc14c7d044dddcc62271ffdbe772ef0e6
mnabil1718 <mnabil1718@gmail.com>
2026-02-27 05:30:45 +0700

    chore: tools and ascii text

--- src/components/ascii.tsx
+++ src/components/ascii.tsx
@@ -1,5 +1,23 @@
-import { Rune } from "rune-ascii";
+import { useAsciiText, slant, ansiShadow } from 'react-ascii-text';
 
 export function Ascii() {
-    return <Rune name="fire" size="s" />
+    const asciiTextRef = useAsciiText({
+        animationCharacters: "▒░█",
+        animationCharacterSpacing: 1,
+        animationDelay: 1000,
+        animationDirection: "down",
+        animationInterval: 100,
+        animationLoop: false,
+        fadeInOnly: true,
+        animationSpeed: 30,
+        font: slant,
+        text: `mnabil`,
+    });
+
+    return <div className="w-full overflow-hidden">
+        <pre
+            ref={asciiTextRef as React.RefObject<HTMLPreElement>}
+            className="origin-left text-base font-bold"
+        />
+    </div>;
 }
