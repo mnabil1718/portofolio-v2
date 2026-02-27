545b60aa2d067f5b25f0ecea4317379015bc339d
mnabil1718 <mnabil1718@gmail.com>
2026-02-27 07:07:16 +0700

    wip: experience

--- src/components/tool.tsx
+++ src/components/tool.tsx
@@ -1,7 +1,7 @@
 export function Tool({ text, url }: { text: string, url: string }) {
     return <a
         href={url}
-        className="hover:text-chart-3 hover:font-semibold hover:before before:content-[''] hover:before:content-['>_']"
+        className="hover:text-primary hover:font-semibold hover:before before:content-[''] hover:before:content-['>_']"
     >{text}</a
     >
 }
