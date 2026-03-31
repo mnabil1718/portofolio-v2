e0c7886b45e91b054ed87de7dd8d375f8caa4144
mnabil1718 <mnabil1718@gmail.com>
2026-03-31 08:28:43 +0700

    Added new route

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+import { defineCollection } from "astro:content";
+import { glob, file } from "astro/loaders";
+import { z } from "astro/zod";
+
+
+const projects = defineCollection({
+    loader: glob({
+        base: "./src/data/projects",
+        pattern: "**/*.md"
+    }),
+    schema: ({ image }) => z.object({
+        title: z.string(),
+        desc: z.string(),
+        image: image(),
+        live_url: z.string().url().optional(),
+        github_url: z.string().url().optional(),
+    })
+})
+
+export const collections = { projects }
---
+++ src/data/projects/matte.md
@@ -0,0 +1,9 @@
+---
+title: "Matté"
+desc: "Matcha & Coffee e-commerce platform with real-time stock and order updates"
+live_url: "https://matte-commerce.vercel.app"
+github_url: "https://github.com/mnabil1718/commerce"
+image: "../../../public/images/matte.png"
+---
+
+hahaha
